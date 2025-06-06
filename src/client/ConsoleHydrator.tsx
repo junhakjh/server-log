2c158288a9f47857bd6888ea554b7a9c985b585d
김준하 <junhakjh@ajou.ac.kr>
2025-06-06 23:38:34 +0900

    chore: add annotation on props: timeout

--- src/client/ConsoleHydrator.tsx
+++ src/client/ConsoleHydrator.tsx
@@ -4,6 +4,14 @@ import { parse } from 'flatted';
 import { useEffect } from 'react';
 
 type Props = {
+  /**
+   * Timeout (in milliseconds) before hydrating logs on the client.
+   *
+   * This should be **slightly greater than** the `timeout` used in `LogScript`,
+   * to ensure the server logs are fully injected into the HTML before reading them.
+   *
+   * default: `1000ms`
+   */
   timeout?: number;
 };
 
--- src/server/LogScript.container.tsx
+++ src/server/LogScript.container.tsx
@@ -2,6 +2,13 @@ import { Suspense } from 'react';
 import { LogScript as LogScriptInjection } from './LogScript';
 
 type Props = {
+  /**
+   * Timeout (in milliseconds) to wait before injecting logs into the HTML.
+   *
+   * This delay ensures all server-side logs are collected before rendering.
+   *
+   * default: `500ms`
+   */
   timeout?: number;
 };
 
